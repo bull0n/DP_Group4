9796dc3ab6fe694fc7005bcb96d662938693c194
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-03-26 10:17:26 +0200

    new classes

---
+++ tp1/src/tp1/parts/PartComponent.java
@@ -0,0 +1,27 @@
+package tp1.parts;
+
+public interface PartComponent {
+	
+	public default void add(PartComponent partComponent) throws Exception
+	{
+		throw new Exception("Can't add a child in a leaf");
+	}
+	
+	public default void remove(PartComponent partComponent) throws Exception
+	{
+		throw new Exception("Can't remove in a leaf");
+	}
+	
+	public default PartComponent getChild(int nChild) throws Exception
+	{
+		throw new Exception("It's a leaf!");
+	}
+	
+	public double getWeight();
+	
+
+	public Dimension3D getDimensions();
+	
+	public String toString();
+
+}
---
+++ tp1/src/tp1/parts/PartComposite.java
@@ -0,0 +1,45 @@
+package tp1.parts;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class PartComposite implements PartComponent {
+
+	public PartComposite() {
+		parts = new LinkedList<PartComponent>();
+	}
+
+	@Override
+	public void add(PartComponent partComponent) throws Exception {
+		this.parts.add(partComponent);
+	}
+
+	@Override
+	public void remove(PartComponent partComponent) throws Exception {
+		this.parts.remove(partComponent);
+	}
+
+	@Override
+	public PartComponent getChild(int nChild) throws Exception {
+		if (parts.size() < nChild) {
+			throw new IndexOutOfBoundsException("nChild is too big");
+		}
+
+		return parts.get(nChild);
+	}
+	
+
+	int getNumberOfElements() {
+		return this.parts.size();
+	}
+	
+
+	public double getWeight() {
+		double w = 0;
+		for (PartComponent p : parts)
+			w += p.getWeight();
+		return w;
+	}
+
+	protected List<PartComponent> parts;
+}
