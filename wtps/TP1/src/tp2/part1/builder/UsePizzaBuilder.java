6caea18767692c7f37e1178e433e1f3157029ca5
Bastien Wermeille <bastien.wermeille@he-arc.ch>
2018-04-16 20:34:19 +0200

    Finalise PizzaBuilder - adding origan, Salami and Pepper

--- wtps/TP1/src/tp2/part1/builder/UsePizzaBuilder.java
+++ wtps/TP1/src/tp2/part1/builder/UsePizzaBuilder.java
@@ -4,6 +4,9 @@ import tp2.part1.concrete.PizzaThin;
 import tp2.part1.decorator.ingredient.DecoratorHam;
 import tp2.part1.decorator.ingredient.DecoratorMozzarella;
 import tp2.part1.decorator.ingredient.DecoratorMushroom;
+import tp2.part1.decorator.ingredient.DecoratorOrigan;
+import tp2.part1.decorator.ingredient.DecoratorPepper;
+import tp2.part1.decorator.ingredient.DecoratorSpicySalami;
 import tp2.part1.decorator.sauce.DecoratorTomato;
 
 public class UsePizzaBuilder {
@@ -15,12 +18,14 @@ public class UsePizzaBuilder {
 	public void main() {
 
 		try {
+			// Margherita : sauce tomate, mozzarella, and origan			
 			PizzaBuilder margueritaBuilder = new PizzaBuilder(PizzaThin.class);
 			margueritaBuilder.setSize(45);
 			margueritaBuilder.sauce(DecoratorTomato.class);
 			margueritaBuilder.addIngredient(DecoratorMozzarella.class);
 			margueritaBuilder.addIngredient(DecoratorOrigan.class);
 
+			// Profunghi : sauce tomate, mozzarella, jambon, champignon
 			PizzaBuilder profunghiBuilder = new PizzaBuilder(PizzaThin.class);
 			profunghiBuilder.setSize(20);
 			profunghiBuilder.sauce(DecoratorTomato.class);
@@ -28,16 +33,13 @@ public class UsePizzaBuilder {
 			profunghiBuilder.addIngredient(DecoratorHam.class);
 			profunghiBuilder.addIngredient(DecoratorMushroom.class);
 			
+			// Diavola : sauce tomate, mozzarella, salami piquant, piment
 			PizzaBuilder diavolaBuilder = new PizzaBuilder(PizzaThin.class);
 			diavolaBuilder.setSize(18);
 			diavolaBuilder.sauce(DecoratorTomato.class);
 			diavolaBuilder.addIngredient(DecoratorMozzarella.class);
 			diavolaBuilder.addIngredient(DecoratorSpicySalami.class);
 			diavolaBuilder.addIngredient(DecoratorPepper.class);
-
-			// Margherita : sauce tomate, mozzarella, and origan
-			// Profunghi : sauce tomate, mozzarella, jambon, champignon
-			// Diavola : sauce tomate, mozzarella, salami piquant, piment
 
 		} catch (Exception e) {
 			System.out.println("Error during pizza cooking");
---
+++ wtps/TP1/src/tp2/part1/decorator/ingredient/DecoratorSpicySalami.java
@@ -0,0 +1,52 @@
+package tp2.part1.decorator.ingredient;
+
+import tp2.enums.Aromas;
+import tp2.enums.Tastes;
+import tp2.part1.Pizza_I;
+
+public class DecoratorSpicySalami extends DecoratorIngredient {
+	
+	public DecoratorSpicySalami(Pizza_I pizza) {
+		super(pizza);
+		this.taste = Tastes.NOT_GOOD;
+		this.aroma = Aromas.SAVOURY;
+		this.price = 0.001f;
+	}
+
+	@Override
+	public float getPrice() {
+		return this.pizza.getPrice()+ this.price * (float)this.pizza.getSize();
+	}
+
+	@Override
+	public boolean isSpicy() {
+		return true;
+	}
+
+	@Override
+	public float getLactose() {
+		return this.pizza.getLactose();
+	}
+
+	@Override
+	public boolean isVegetarian() {
+		return this.pizza.isVegetarian();
+	}
+
+	@Override
+	public String getTaste() {
+		return this.pizza.getTaste() + this.taste.toString();
+	}
+
+	@Override
+	public String getAroma() {
+		return this.pizza.getAroma() + this.aroma.toString();
+	}
+
+	@Override
+	public String toString() {
+		return this.pizza.toString() + " mushroom";
+	}
+
+	
+}
